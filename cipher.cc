feb544b077b2e8889a294e8c3e5dea994211b2f2
Matt Miller <matthew.c.anthony.miller@gmail.com>
2016-08-04 20:44:25 -0500

    updated cipher.cc

--- cipher.cc
+++ cipher.cc
@@ -3,60 +3,73 @@
 #include <string>
 #include <map>
 
-int main(int argc, char* argv[]){
+using namespace std;
+
+int main(int argc, char** argv){
 	unsigned int LOWEST_ASCII = 32;
 	unsigned int HIGHEST_ASCII = 126;
 	//take an input file, an output file and a word
+
 	if(argc > 5 || argc < 3){
-		std::cout << "Usage: " << argv[0] << " [input file] " << "[keyword] " << "[output file (optional)] " << "-d (decrypt, optional)";
+		cout << "Usage: " << argv[0] << " [input file] " << "[keyword] " << "[output file (optional)] " << "-d (decrypt, optional)\n";
 		return 0;
 	}
 
-	std::ofstream inFile;
-	std::ofstream outFile;
-	std::string codeword;
+	ifstream inFile;
+	FILE * outFile;
+	string codeword;
 	char c;
 	unsigned int char_num = 0;
 	char raw_char;
 	char new_c;
 	bool encrypt_mode;
 
-	if(strcmp("-d", argv[4]) == 0 || strcmp("-d", argv[5]) == 0){					//decrypt mode
+	for(int i = 0; i < argc; i++){
+		cout << argv[i] << "\n";
+	}
+
+	if(argc >= 4 && (strcmp("-d", argv[3]) == 0 || strcmp("-d", argv[4]) == 0)){					//decrypt mode
 		encrypt_mode = false;
 	} else {															//could just be an outfile
 		encrypt_mode = true;
 	}
 
-	inFile.open(argv[2]);
-	codeword = argv[3];
+	inFile.open(argv[1]);
+	codeword = argv[2];
 	
-	std::map <char, unsigned int> alpha;
-	for(int letter = LOWEST_ASCII; letter < HIGHEST_ASCII; letter++){	//initialize 'alphabet'
+	map <char, unsigned int> alpha;
+	for(char letter = LOWEST_ASCII; letter < HIGHEST_ASCII; letter++){	//initialize 'alphabet'
 		alpha[letter] = 0;
+		cout << letter << "\t";
+		if(letter % 5 == 0) cout << endl;
 	}
 
 	if(argv[4] && strcmp("-d", argv[4]) == 0 ){
-		outFile.open(argv[4]);
+		outFile = fopen(argv[3], "w");
 	} else {
-		outFile.open("output.txt");
+		outFile = fopen("output.txt", "w");
 	}
 
-	if(!inFile.is_open() || !outFile.is_open()){
-		std::cout << "Error opening a file.";
+	if(!inFile.is_open() || outFile == NULL){
+		cout << "Error opening a file.\n";
+		!inFile.is_open() ? cout << "it was in.\n" : cout << "it was out.\n";
 		return -1;
 	}
 	if(encrypt_mode){
-
-		while(inFile >> c){
+		while(inFile.get(c)){
 			raw_char = c + codeword[char_num % codeword.length()];
-			new_c = alpha[raw_char % (alpha.size())];
-			alpha[new_c]++;
+			//cout << "*** " << raw_char << " ***\n";
+			new_c = raw_char % alpha.size();
+			alpha[new_c] = alpha[new_c] + 1;
+			cout << new_c << "\n";
 			char_num++;
+			fputc(new_c, outFile);
 		}
+		fclose(outFile);
 
-		std::cout << "COUNTS:";
-		for(auto i : alpha)
-			std::cout << i.first << ": " << i.second << std::endl;
-
+		cout << "COUNTS:";
+		for(auto i : alpha){
+			cout << i.first << ": " << i.second << endl;
+		}
 	}
 }
